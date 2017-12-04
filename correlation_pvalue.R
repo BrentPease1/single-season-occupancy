4ec3f8011b7bb104286ca513a37a93949c13a7d8
BrentPease1 <bspease@ncsu.edu>
2017-12-04 12:18:27 -0500

    function to compute p-values in correlation plot

---
+++ correlation_pvalue.R
@@ -0,0 +1,23 @@
+#compute the p-value of correlations
+#http://www.sthda.com/english/wiki/visualize-correlation-matrix-using-correlogram
+
+
+# mat : is a matrix of data
+# ... : further arguments to pass to the native R cor.test function
+cor.mtest <- function(mat, ...) {
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      tmp <- cor.test(mat[, i], mat[, j], ...)
+      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  p.mat
+}
+# matrix of the p-value of the correlation
+p.mat <- cor.mtest(mtcars)
+head(p.mat[, 1:5])
