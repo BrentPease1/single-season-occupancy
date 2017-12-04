bffc4ba73139690fe5794d5a181f1553c4a5d3e9
BrentPease1 <bspease@ncsu.edu>
2017-12-04 14:26:04 -0500

    added all correlation plot lines to this script

--- correlation_pvalue.R
+++ correlation_pvalue.R
@@ -1,6 +1,6 @@
 #compute the p-value of correlations
 #http://www.sthda.com/english/wiki/visualize-correlation-matrix-using-correlogram
-
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
 
 # mat : is a matrix of data
 # ... : further arguments to pass to the native R cor.test function
@@ -21,3 +21,13 @@ cor.mtest <- function(mat, ...) {
 # matrix of the p-value of the correlation
 p.mat <- cor.mtest(mtcars)
 head(p.mat[, 1:5])
+
+corrplot(cov.cor, method="color", col=col(200),  
+         type="upper", order="hclust", 
+         addCoef.col = "black", # Add coefficient of correlation
+         tl.col="black", tl.srt=45, #Text label color and rotation
+         # Combine with significance
+         p.mat = p.mat, sig.level = 0.05, insig = "blank", 
+         # hide correlation coefficient on the principal diagonal
+         diag=TRUE 
+)
