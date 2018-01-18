b4a4425f1b3addc3538391ebf0077feb3bf1dde9
Brent Pease <bspease@ncsu.edu>
2018-01-18 15:25:02 -0500

    removed s neighborhood

--- create_grid.R
+++ create_grid.R
@@ -17,9 +17,8 @@ for(i in 1:-1)
   for(j in 1:-1)
     if(i!=0 || j !=0)
       N<-rbind(N,m2[addresses$x+i+1+nrow(m2)*(addresses$y+j)])
-N <- t(N)                                        #transpose output
-
-#rm(m2, mat, i, j)                                #clean up environemnt
+N <- t(N)                                                      #transpose output
+N <- t(apply(N, 1, function(x)sort(x, na.last = TRUE)))        #sort output
 
 
 #Calculate cardinality of each cell
@@ -37,11 +36,11 @@ nsurveys <- 4
 nyears <- 1
 
 #define parameter values
-alpha <- -0.5
-beta <- 1.5
+alpha <- -1
+beta <- 2.5
 
 
-#define occupancy across sites
+#define initial occupancy across sites
 psi <- 0.4
 
 
@@ -50,39 +49,57 @@ p <- 0.3
 
 
 #Create arrays to hold true occurrence status and sampling realizations 
+w <- array(NA, dim = c(nsites,nyears))
 z <- array(NA, dim = c(nsites,nyears)) 
-y <- array(NA, dim = c(nsites,nsurveys))
+y <- matrix(NA, nrow=nsites, ncol = nsurveys)
+tmp <- matrix(NA, nrow=nsites, ncol = nsurveys)
 
 
-#Generate presence/absence (truth)
-z <- rbinom(mat.row*mat.col,1,psi)
+#Generate initial presence/absence to build neighborhood structure off of
+for(i in unique(N)){
+w <- rbinom(mat.row*mat.col,1,psi)}
 
-#Calculate autocovariate
-x <- array(NA, dim = c(mat.row*mat.col, nyears))
+#Calculate neighborhood autocovariate
+x <- array(NA, dim = c(mat.row*mat.col, max(numnn)))
 for(i in 1:nG){
-  for(j in 1:numnn[i]){
-    x[i] <- sum(z[N[i,]], na.rm = TRUE)/numnn[i]
-  }   
-}
+  x[i,1]<-0
+  for(j in 2:numnn[i]){
+    x[i,j]<-x[i,j-1]+w[N[i,j-1]]
+  }}
+
+#calculate psi from the initial state layer of presence/absence
+for(i in 1:nG){
+psi[i] <- plogis(alpha + beta*(x[i,numnn[i]]/numnn[i]))}
+
+#Generate true presence/absence across grid
+for(i in 1:nG){
+z <- rbinom(nG, 1, psi[i])}
+
+#Generate detection/non-detection data (observed)
+for(i in 1:nG){
+  mu <- z[i]*p
+for(j in 1:nsurveys){
+  y[i,j] <- rbinom(1, 1, mu)
+}}
 
 
 #package up objects into a list
-grid.data <- list(z=z,numnn=numnn,N = N, nG = nG, griddim = griddim, x=x)
+grid.data <- list(z=z,y=y,numnn=numnn,N = N, nG = nG, griddim = griddim, x=x)
 
 #Visualize the truth
 Zmat<-matrix(NA,nrow=mat.row,ncol=mat.col) #first, create a matrix to hold values of z
-Zmat[1:nG]<-z                              #assign values of z to matrix
+z2 <- as.vector(as.integer(z))
+Zmat[1:nG]<- z2                             #assign values of z to matrix
 
 #plot
 par(cex.axis=1.8,cex.lab=2.0,cex.main=2,mar= c(5, 4, 4, 2)*1.2 + 0.1)
 image(1:mat.row,1:mat.col,Zmat,col=rev(terrain.colors(10)),xlab="Easting",ylab="Northing",main = "True presence/absence across grid")
 
 #Visualize detection/non-detection
-p = 0.3
-y<-rbinom(length(z),1,p*z)
 Ymat<-matrix(NA,nrow=griddim,ncol=griddim)
-Ymat[1:nG]<-y
+Ymat[1:nG]<- as.integer(apply(y,1,max))
 
 par(cex.axis=1.8,cex.lab=2.0,cex.main=2,mar= c(5, 4, 4, 2)*1.2 + 0.1)
 image(1:mat.row,1:mat.col,Ymat,col=rev(terrain.colors(10)),xlab="Easting",ylab="Northing",main = "Detection/non-detection across grid")
 
+rm(tmp,mu,nsites,nsurveys,nyears,w,s,addresses, m2, mat, N, x, y, Ymat, Zmat, alpha, beta, griddim, i, j, mat.col, mat.row, nG, numnn, p, psi, z, z2)
